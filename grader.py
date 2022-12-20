dcaf79458f7873cfc5e73059a6aaff0db8cad1fe
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2022-12-20 11:03:00 +0530

    Count the no  of files in puzzels folder and itarate thorugh them. Then run the exe file and cheack the output by input them stdio

---
+++ grader.py
@@ -0,0 +1,34 @@
+import subprocess
+import os
+
+EXEFILE =  "puzzel1.exe"  # Exe File Name
+
+def filesize():
+    count = 0
+    for root,dir,files in os.walk("puzzels/"):
+        count += len(files)
+    return count
+    
+def main():
+    FILE = filesize()
+    for i in range(FILE):
+        puzzle  = open("puzzels/"+"puzzel"+ str(i+1)+".txt",'r')
+        answer = open("answer/"+"answer"+ str(i+1)+".txt",'r')
+        
+        data = "".join(puzzle.readlines())
+        data2= "".join(answer.readlines())
+        
+        print("Test Case",i+1,"\n\nInput:")
+        print(data,"\nOutput:")
+        out = subprocess.run([EXEFILE,"-","-"],shell=True,input=data.encode(),capture_output=True)
+        print(out.stdout.decode())
+        print("\nExpected:")
+        print(data2)
+        print("\nReturn Code:",out.returncode,"\n")
+        print("************************************************","\n")
+        
+        puzzle.close()
+        answer.close()
+
+if __name__ == '__main__':
+    main()
